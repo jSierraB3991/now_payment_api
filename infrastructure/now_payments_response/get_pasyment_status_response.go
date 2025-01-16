56266cd0c931960e1b9ce02fa8998c27a91aa47b
Eliot Anderson <eliotandelon@gmail.com>
2025-01-16 16:18:24 -0500

    add get status by invoice url

---
+++ infrastructure/now_payments_response/get_pasyment_status_response.go
@@ -0,0 +1,28 @@
+package nowpaymentsresponse
+
+import "time"
+
+type GetPaymentStatusResponse struct {
+	PaymentId        string    `json:"payment_id"`
+	InvoiceId        *string   `json:"invoice_id"`
+	PaymentStatus    string    `json:"payment_status"`
+	PayAddress       string    `json:"pay_address"`
+	PayInExtraId     *string   `json:"payin_extra_id"`
+	PriceAmount      float32   `json:"price_amount"`
+	PriceCurrency    string    `json:"price_currency"`
+	PayAmount        float32   `json:"pay_amount"`
+	ActualPaid       string    `json:"actually_paid"`
+	PayCurrency      string    `json:"pay_currency"`
+	OrderId          *uint     `json:"order_id"`
+	OrderDescription *string   `json:"order_description"`
+	PurchaseId       uint      `json:"purchase_id"`
+	OutComeAmount    float32   `json:"outcome_amount"`
+	OutComeCurrency  string    `json:"outcome_currency"`
+	PayoutHash       string    `json:"payout_hash"`
+	PayinHash        string    `json:"payin_hash"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
+	BurningPercent   string    `json:"burning_percent"`
+	Type             string    `json:"type"`
+	PaymentExtraIds  []int64   `json:"payment_extra_ids"`
+}
--- infrastructure/now_payments_service/get_invoice_status_service.go
+++ infrastructure/now_payments_service/get_invoice_status_service.go
@@ -1,10 +1,14 @@
 package nowpaymentsservice
 
-func (s *NowPaymentService) GetInvoiceStatus(invoiceId string) error {
-	var result interface{}
+import (
+	nowpaymentsresponse "github.com/jSierraB3991/now_payment_api/infrastructure/now_payments_response"
+)
+
+func (s *NowPaymentService) GetInvoiceStatus(invoiceId string) (*nowpaymentsresponse.GetPaymentStatusResponse, error) {
+	var result nowpaymentsresponse.GetPaymentStatusResponse
 	err := s.httpClient.Get(s.apiUrl, ""+invoiceId, s.apiKey, &result)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+	return &result, nil
 }
