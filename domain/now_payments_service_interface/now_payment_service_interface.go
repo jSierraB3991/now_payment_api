348996db02969bb29af02ee141e543dbabc4d1d1
Juan David Sierra <judas3991@gmail.com>
2025-06-09 12:20:21 -0500

    add remove user and password for now payments

--- domain/now_payments_service_interface/now_payment_service_interface.go
+++ domain/now_payments_service_interface/now_payment_service_interface.go
@@ -12,7 +12,6 @@ type NowPaymentServiceInterface interface {
 	CreatePayment(ctx context.Context, req nowpaymentsrequest.CreatePaymentRequest, userId uint, apiKey string) (*nowpaymentsresponse.CreatePaymentResponse, error)
 	CreateInvoice(ctx context.Context, req nowpaymentsrequest.CreateInvoiceRequest, userId uint, payCurrency, apiKey string) (*nowpaymentsresponse.CreateInvoiceResponse, error)
 	GetInvoiceDataService(ctx context.Context, page, limit int, isAscendente bool, userId uint) ([]nowpaymentsmodel.NowPaymentCreateInvoice, error)
-	GetPaymentStatus(ctx context.Context, invoiceId, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error)
-	GetInvoiceStatus(ctx context.Context, invoiceId, userName, password, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error)
+	GetPaymentStatus(ctx context.Context, invoiceId uint, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error)
 	GetInvoiceByOrderIdService(ctx context.Context, orderId string) (*nowpaymentsresponse.InvoiceResponse, error)
 }
--- infrastructure/now_payments_service/get_payment_status_service.go
+++ infrastructure/now_payments_service/get_payment_status_service.go
@@ -3,12 +3,13 @@ package nowpaymentsservice
 import (
 	"context"
 
+	eliotlibs "github.com/jSierraB3991/jsierra-libs"
 	nowpaymentlibs "github.com/jSierraB3991/now_payment_api/domain/now_payment_libs"
 	nowpaymentsrequest "github.com/jSierraB3991/now_payment_api/infrastructure/now_payments_request"
 	nowpaymentsresponse "github.com/jSierraB3991/now_payment_api/infrastructure/now_payments_response"
 )
 
-func (s *NowPaymentService) GetPaymentStatus(ctx context.Context, invoiceId, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error) {
+func (s *NowPaymentService) GetPaymentStatus(ctx context.Context, invoiceId uint, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error) {
 
 	headers := []nowpaymentsrequest.HeaderRequest{{
 		Key:   "x-api-key",
@@ -16,7 +17,7 @@ func (s *NowPaymentService) GetPaymentStatus(ctx context.Context, invoiceId, api
 	}}
 
 	var result nowpaymentsresponse.GetPaymentStatusResponse
-	err := s.httpClient.Get(s.apiUrl, nowpaymentlibs.GET_PAYMENT_DATA_URL+invoiceId, &result, headers)
+	err := s.httpClient.Get(s.apiUrl, nowpaymentlibs.GET_PAYMENT_DATA_URL+eliotlibs.GetStringUNumberFor(invoiceId), &result, headers)
 	if err != nil {
 		return nil, err
 	}
