24eed64abd1b6937f5580cba1ba4d1bc164e9f0b
Juan David Sierra <judas3991@gmail.com>
2025-07-15 17:21:42 -0500

    add pagination data for default

--- domain/now_payments_service_interface/now_payment_service_interface.go
+++ domain/now_payments_service_interface/now_payment_service_interface.go
@@ -12,7 +12,7 @@ import (
 type NowPaymentServiceInterface interface {
 	CreatePayment(ctx context.Context, req nowpaymentsrequest.CreatePaymentRequest, userId uint, apiKey string) (*nowpaymentsresponse.CreatePaymentResponse, error)
 	CreateInvoice(ctx context.Context, req nowpaymentsrequest.CreateInvoiceRequest, userId uint, payCurrency, apiKey string) (*nowpaymentsresponse.CreateInvoiceResponse, error)
-	GetInvoiceDataService(ctx context.Context, page, limit int, isAscendente bool, params []eliotlibs.PagginationParam) ([]nowpaymentsmodel.NowPaymentCreateInvoice, error)
+	GetInvoiceDataService(ctx context.Context, page *eliotlibs.Paggination, params []eliotlibs.PagginationParam) ([]nowpaymentsmodel.NowPaymentCreateInvoice, error)
 	GetPaymentStatus(ctx context.Context, invoiceId, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error)
 	GetInvoiceStatus(ctx context.Context, invoiceId, userName, password, apiKey string) (*nowpaymentsresponse.GetPaymentStatusResponse, error)
 	GetInvoiceByOrderIdService(ctx context.Context, orderId string) (*nowpaymentsresponse.InvoiceResponse, error)
--- infrastructure/now_payments_service/get_invoice_data_service.go
+++ infrastructure/now_payments_service/get_invoice_data_service.go
@@ -4,18 +4,12 @@ import (
 	"context"
 
 	eliotlibs "github.com/jSierraB3991/jsierra-libs"
-	nowpaymentsmapper "github.com/jSierraB3991/now_payment_api/domain/now_payments_mapper"
 	nowpaymentsmodel "github.com/jSierraB3991/now_payment_api/domain/now_payments_model"
 )
 
-func (s *NowPaymentService) GetInvoiceDataService(ctx context.Context, page, limit int, isAscendente bool, params []eliotlibs.PagginationParam) ([]nowpaymentsmodel.NowPaymentCreateInvoice, error) {
+func (s *NowPaymentService) GetInvoiceDataService(ctx context.Context, page *eliotlibs.Paggination, params []eliotlibs.PagginationParam) ([]nowpaymentsmodel.NowPaymentCreateInvoice, error) {
 
-	order := "desc"
-	if isAscendente {
-		order = "asc"
-	}
-	paggination := nowpaymentsmapper.PaginationReqToModel(limit, page, order, "order_id", nil)
-	data, err := s.repository.GetInvoicePagination(ctx, paggination, params)
+	data, err := s.repository.GetInvoicePagination(ctx, page, params)
 	if err != nil {
 		return nil, err
 	}
